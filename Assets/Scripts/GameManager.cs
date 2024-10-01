958478a9a74c4e756a3f7f8435471bb52732621d
cssmith8 <cssmith4509@gmail.com>
2024-09-30 23:51:07 -0500

    variable examples

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,13 @@
 using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Security.Cryptography;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+
+//set hashtable
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
@@ -27,10 +32,53 @@ public class GameManager : MonoBehaviourPunCallbacks
 
     }
 
-    // Update is called once per frame
+    
+
+    void PlayerVariableSetExample()
+    {
+        Hashtable customProperties = PhotonNetwork.LocalPlayer.CustomProperties;
+
+        //set player-specific variable
+        customProperties["variablename"] = "can put any primitive type here";
+        PhotonNetwork.LocalPlayer.SetCustomProperties(customProperties);
+
+        //changes are inconsistent if you set the hash multiple times in one frame. dont set player variables every frame.
+        //this isnt stated anywhere in the documentation
+        //i had to post help on the photon forums to figure this out
+        //and even then nobody knew and i had to figure it out myself
+    }
+
+    void PlayerVariableGetExample()
+    {
+        Hashtable customProperties = enemypv.Owner.CustomProperties;
+
+        //get player-specific variable
+        string real = (string)customProperties["variablename"];
+    }
+
+    void RoomVariableExample()
+    {
+        Hashtable hash = PhotonNetwork.CurrentRoom.CustomProperties;
+
+        //set room variable
+        hash["variablename"] = "can put any primitive type here";
+        PhotonNetwork.CurrentRoom.SetCustomProperties(hash);
+
+        //get room variable
+        string real = (string)hash["variablename"];
+
+        //see above comments on not changing variables every frame. I assume it's the same for room variables.
+    }
+
+
+    // -------------
+
+
+    //RPCs - used to call functions across clients
+
     void Update()
     {
-        //when r key is pressed
+        //when r key is pressed. just for example
         if (Input.GetKeyDown(KeyCode.R))
         {
             RPCExample();
