3babb593dbb7baf9bf6de0143cfbe3d31e1444e4
KZhang2002 <kennethzhang2002@gmail.com>
2024-11-14 11:17:20 -0600

    Renamed cards. Added json data.

--- Assets/Scripts/HandManager.cs
+++ Assets/Scripts/HandManager.cs
@@ -4,58 +4,58 @@ using UnityEngine;
 
 namespace DefaultNamespace {
     public class HandManager : MonoBehaviour {
-        /*
-        //Singleton
-        [HideInInspector] public static HandManager localInstance;
+         /*
+         //Singleton
+         [HideInInspector] public static HandManager localInstance;
 
-        private List<DisplayManager> dmList = new List<DisplayManager>();
-        public Branch cardFamily;
-        [HideInInspector] public int selectedCardIndex = 0;
-        
-        private void Start() {
-            localInstance = this;
-            // todo replace grab children method with something more specific
-            for (int i = 0; i < transform.childCount; i++) {
-                var childSR = transform.GetChild(i)
-                    .GetChild(1)
-                    .GetChild(0)
-                    .GetComponent<DisplayManager>() ?? null;
-                if (childSR) {
-                    dmList.Add(childSR);
-                }
-            }
-        }
+         private List<DisplayManager> dmList = new List<DisplayManager>();
+         public Branch cardFamily;
+         [HideInInspector] public int selectedCardIndex = 0;
+         
+         private void Start() {
+             localInstance = this;
+             // todo replace grab children method with something more specific
+             for (int i = 0; i < transform.childCount; i++) {
+                 var childSR = transform.GetChild(i)
+                     .GetChild(1)
+                     .GetChild(0)
+                     .GetComponent<DisplayManager>() ?? null;
+                 if (childSR) {
+                     dmList.Add(childSR);
+                 }
+             }
+         }
 
-        public void setImages(Branch branch) {
-            cardFamily = branch;
-            foreach (DisplayManager dm in dmList)
-            {
-                dm.setImage(branch);
-            }
-        }
+         public void setImages(Branch branch) {
+             cardFamily = branch;
+             foreach (DisplayManager dm in dmList)
+             {
+                 dm.setImage(branch);
+             }
+         }
 
-        public void incrementBranch() {
-            Branch branchToSet = (Branch)(((int)cardFamily % 5) + 1); // todo ew lol
-            cardFamily = branchToSet;
-            setImages(branchToSet);
-        }
-        
-        public void decrementBranch() {
-            Branch branchToSet = (Branch)((((int)cardFamily + 3) % 5) + 1); // todo ew lol
-            cardFamily = branchToSet;
-            setImages(branchToSet);
-        }
+         public void incrementBranch() {
+             Branch branchToSet = (Branch)(((int)cardFamily % 5) + 1); // todo ew lol
+             cardFamily = branchToSet;
+             setImages(branchToSet);
+         }
+         
+         public void decrementBranch() {
+             Branch branchToSet = (Branch)((((int)cardFamily + 3) % 5) + 1); // todo ew lol
+             cardFamily = branchToSet;
+             setImages(branchToSet);
+         }
 
-        public void hideCards()
-        {
-            // for every child
-            for (int i = 0; i < transform.childCount; i++)
-            {
-                if (i == selectedCardIndex) continue;
-                transform.GetChild(i).GetChild(1).GetComponent<LocalCardSlotSlot>().Hide();
-                GameManager.localInstance.HideRole(i);
-            }
-        }
-        */
+         public void hideCards()
+         {
+             // for every child
+             for (int i = 0; i < transform.childCount; i++)
+             {
+                 if (i == selectedCardIndex) continue;
+                 transform.GetChild(i).GetChild(1).GetComponent<LocalCardSlotSlot>().Hide();
+                 GameManager.localInstance.HideRole(i);
+             }
+         }
+         */
     }
 }
